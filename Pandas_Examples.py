82272253c545eaab1fcf2a1f80adecc1647c1f70
mjs3401 <104317461+mjs3401@users.noreply.github.com>
2022-04-24 14:10:26 -0700

    Pandas dataframe examples viewed in pycharm dataframe view

--- Pandas_Examples.py
+++ Pandas_Examples.py
@@ -28,6 +28,35 @@ def main():
     # Melt: Gather columns into rows. Rename new columns.
     df_melt = pd.melt(df).rename(columns={'variable':'var', 'value':'val'})
 
+    # Concat dataframes
+    # Concat two
+    df_concat = pd.concat([df,df])
+
+    # Subset rows
+    # Select rows
+    df_rows = df[df.sepal_length > 5]
+    # Select sample of rows
+    df_sample = df.sample(frac=0.5)
+    # Select 10 largest
+    df_largest = df.nlargest(10,'sepal_length')
+    # Select 10 smallest
+    df_smallest = df.nsmallest(10, 'sepal_length')
+    # First n rows
+    df_head = df.head(25)
+    # Select last n rows
+    df_tail = df.tail(25)
+
+    # Subset columns
+    # Select subset of mulitple columns
+    df_subcol = df[['species', 'sepal_length']]
+    # Select single column
+    df_onecol = df['species']
+    # Other way to do this
+    df_onecol = df.species
+    # Select column names that match regex
+    df_regex_col = df.filter(regex='.*\width$')
+
+
 
     return
 
